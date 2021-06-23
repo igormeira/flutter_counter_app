c66bb03db067d7d0d39a786fd6620a86a917d8be
Igor Meira <igormeira.dev@gmail.com>
2021-06-22 22:28:03 -0300

    Simple counter

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -1,5 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container());
+  runApp(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Primeiro app flutter"),
+      ),
+      body:
+          Center(child: Text("CONTADOR\n$count", textAlign: TextAlign.center)),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
 }
